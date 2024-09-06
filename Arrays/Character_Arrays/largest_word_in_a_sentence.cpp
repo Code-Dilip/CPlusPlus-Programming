3e04770843a9565b6c7f225768ab41945eb089fe
Code-Dilip <learndilip05@gmail.com>
2024-09-07 00:07:16 +0530

    c

--- Arrays/Character_Arrays/largest_word_in_a_sentence.cpp
+++ Arrays/Character_Arrays/largest_word_in_a_sentence.cpp
@@ -1,40 +1,38 @@
 #include<iostream>
 #include<climits>
+#include<string>
+
 using namespace std;
 
-void print_char_array(char str[]){
-    int i=0;
-    while(str[i] != '\0'){
-        cout<<str[i];
-        i++;
+void largest_word(string str){
+    int max_len=INT_MIN,current_len=0,n=str.size(),st_idx=0,max_st_idx=0;
+    for(int i=0;i<n;i++){
+        if(str[i]==' ' || str[i]=='\0'){
+            if(current_len > max_len){
+                max_len = current_len;
+                max_st_idx = st_idx;
+            }
+            current_len = 0;
+            st_idx = i+1;
+        }
+        else{
+            current_len ++;
+        }
+    }
+    cout<<"Largest word in the sentence is ";
+    for(int i=0;i<max_len;i++){
+        cout<<str[max_st_idx+i];
     }
     cout<<endl;
 }
 
 int main(){
-    int n;
-    cout<<"\nenter the size of the sentence: ";
-    cin>>n;
-    char str[n+1];
+    string str;
+    cout<<"\nenter a sentence to find largest word in it: ";
+    getline(cin,str);
+    cout<<"The entered string is "<<str<<endl;
 
-    cout<<"enter the sentence: ";
-    cin.getline(str,n+1);
-    cin.ignore();
-
-    cout<<endl;
-
-    int max_length = INT_MIN,curr_legth = 0;
-    for(int i=0;i<n;i++){
-        if(str[i]==' ' || str[i]=='\0'){
-            max_length = max(max_length,curr_legth);
-            curr_legth = 0;
-        }
-        else{
-            curr_legth ++;
-        }
-    }
-
-    cout<<"Length of the largest word is "<<max_length<<" ."<<endl;
+    largest_word(str);
 
     cout<<endl;
     return 0;
