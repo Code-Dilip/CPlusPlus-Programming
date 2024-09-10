0bca7fa1f53aef0cee868c3c8df0af93c3b3e3cd
Code-Dilip <learndilip05@gmail.com>
2024-09-10 21:18:23 +0530

    bit manipulation

--- Bit_Manupulation/Clear_Bit.cpp
+++ Bit_Manupulation/Clear_Bit.cpp
@@ -10,12 +10,18 @@ int setBit(int n,int pos){
     return (n|(1<<pos));
 }
 
+int clearBit(int n,int pos){
+    return (n&(~(1<<pos)));
+}
+
 int main(){
-    int n=5,pos=1;
+    int n=5,pos=2;
     cout<<endl;
     cout<<getBit(n,pos)<<endl;
     cout<<setBit(n,pos)<<endl;
     n = setBit(n,pos);
+    cout<<getBit(n,pos)<<endl;
+    n = clearBit(n,pos);
     cout<<getBit(n,pos)<<endl;
     return 0;
 }
--- Bit_Manupulation/Update_Bit.cpp
+++ Bit_Manupulation/Update_Bit.cpp
@@ -10,12 +10,24 @@ int setBit(int n,int pos){
     return (n|(1<<pos));
 }
 
+int clearBit(int n,int pos){
+    return (n&(~(1<<pos)));
+}
+
+int updateBit(int n,int pos,bool value){
+    n = (n&(~(1<<pos)));
+    return (n|(value<<pos));
+}
+
 int main(){
-    int n=5,pos=1;
+    int n=5,pos=2,value=1;
     cout<<endl;
     cout<<getBit(n,pos)<<endl;
     cout<<setBit(n,pos)<<endl;
     n = setBit(n,pos);
     cout<<getBit(n,pos)<<endl;
+    n = clearBit(n,pos);
+    cout<<getBit(n,pos)<<endl;
+    cout<<updateBit(n,pos,value)<<endl;
     return 0;
 }
