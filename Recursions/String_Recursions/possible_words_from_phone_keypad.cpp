d1343c1060956bb2034a7b89a2a18ab980ee7413
Code-Dilip <learndilip05@gmail.com>
2024-09-12 23:39:06 +0530

    string recursions

--- Recursions/String_Recursions/possible_words_from_phone_keypad.cpp
+++ Recursions/String_Recursions/possible_words_from_phone_keypad.cpp
@@ -3,7 +3,23 @@
 
 using namespace std;
 
+string keyPadArray[] = {"","./","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+void printPossibleWords(string str,string ans){
+    if(!str.length()){
+        cout<<ans<<endl;
+        return;
+    }
+    char ch = str[0];
+    string word = keyPadArray[ch-'0'];
+    for(int i=0;i<word.length();i++){
+        printPossibleWords(str.substr(1),ans+word[i]);
+    }
+}
+
 int main(){
-    string str;
-    
+    cout<<endl;
+    printPossibleWords("23","");
+    cout<<endl;
+    return 0;
 }
