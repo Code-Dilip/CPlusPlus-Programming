e62cedc5a0897cbd610447cdf7374f8a7109096f
Code-Dilip <learndilip05@gmail.com>
2024-09-21 22:23:29 +0530

    vectors and paris

---
+++ Vectors and pairs/pairs.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    pair<int,char>p;
+    p.first=1;
+    p.second='2';
+    cout<<"\np.first: "<<p.first<<", p.second: "<<p.second<<" "<<endl;
+    return 0;
+}
