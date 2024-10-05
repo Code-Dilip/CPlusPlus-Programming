f167d445b7e06de21358ca039fad6a7e5eb94814
Code_Dilip <learndilip05@gmail.com>
2024-10-05 22:24:13 +0530

    stack implementation

--- Stacks/Stack_Implitation/stack_implimentation_using_array.cpp
+++ Stacks/Stack_Implitation/stack_implimentation_using_array.cpp
@@ -1,24 +1,74 @@
 #include<iostream>
-
-int const n=100;
-int arr[n];
+#define n 10
+using namespace std;
 
 class stack{
     public:
-    int top = 0;
-    void push(int val){
-        top ++;
-        arr[top] = val;
+    int *arr;
+    int top = -1;
+    stack(){
+        arr = new int[n];
+        top = -1;
     }
+
+    void push(int x){
+        if(top>=n){
+            cout<<"Stack Overflow!"<<endl;
+            return ;
+        }
+        top ++;
+        arr[top] = x;
+        return ;
+    }
+
     void pop(){
+        if(top<=-1){
+            cout<<"Stack is empty!"<<endl;
+            return ;
+        }
         top --;
     }
-    int top(){
+
+    int seek(){
+        if(top <= -1){
+            cout<<"Stack is empty!"<<endl;
+            return 0;
+        }
         return arr[top];
     }
-    bool isEmpty()
-}
+
+    bool isEmpty(){
+        if(top == -1){
+            return 1;
+        }
+        return 0;
+    }
+
+    void print(){
+        if(top<=-1 || top>=n){
+            return ;
+        }
+        for(int i=0;i<=top;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+        return ;
+    }
+};
 
 int main(){
+    stack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(6);
+    s.push(7);
+    s.push(8);
+    s.push(9);
+    s.push(10);
+    s.push(11);
+    s.print();
     return 0;
 }
--- Stacks/Stack_Implitation/stacks_using_linked_list.cpp
+++ Stacks/Stack_Implitation/stacks_using_linked_list.cpp
@@ -2,7 +2,92 @@
 
 using namespace std;
 
+class node{
+    public:
+    int data;
+    node *prev;
+    node *next;
+    node(int val){
+        data = val;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+class stack{
+    public:
+    node *top;
+    stack(){
+        top = nullptr;
+    }
+
+    void push(int val){
+        node *newp = new node(val);
+        if(top == nullptr){
+            top = newp;
+            return ;
+        }
+        top->next = newp;
+        newp->prev = top;
+        top = newp;
+    }
+    
+    void pop(){
+        if(top==nullptr){
+            cout<<"Stack is Empty!"<<endl;
+            return ;
+        }
+        node *temp = top;
+        top = top->prev;
+        free(temp);
+    }
+
+    int seek(){
+        if(top==nullptr){
+            cout<<"Stack is Empty!"<<endl;
+            return 0;
+        }
+        return top->data;
+    }
+
+    bool isEmpty(){
+        if(top == nullptr){
+            return 1;
+        }
+        return 0;
+    }
+
+    void print(){
+        node *temp = top;
+        while(temp != nullptr){
+            cout<<temp->data<<" ";
+            temp = temp->prev;
+        }
+        cout<<endl;
+    }
+};
+
 int main(){
-    cout<<"\nHello World!"<<endl;
+    stack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.print();
+    cout<<s.isEmpty()<<endl;
+
+    s.pop();
+    s.pop();
+    s.print();
+    cout<<s.seek()<<endl;
+
+    s.pop();
+    s.pop();
+    s.pop();
+    s.print();
+    cout<<s.seek()<<endl;
+    cout<<s.isEmpty()<<endl;
+
     return 0;
 }
