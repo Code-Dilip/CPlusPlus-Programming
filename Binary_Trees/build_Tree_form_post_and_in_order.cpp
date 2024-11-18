4d8cbb15cfca30553e306767dc8f31bddb41f160
Code_Dilip <learndilip05@gmail.com>
2024-11-18 15:33:29 +0530

    build tree from post and in order

---
+++ Binary_Trees/build_Tree_form_post_and_in_order.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val){
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void inOrderTraversal(struct Node *root){
+    if(root == nullptr){
+        return ;
+    }
+    inOrderTraversal(root->left);
+    cout<<root->data<<" ";
+    inOrderTraversal(root->right);
+}
+
+int search_pos(int inOrder[],int start,int end,int curr){
+    for(int i=start;i<=end;i++){
+        if(inOrder[i] == curr){
+            return i;
+        }
+    }
+    return  -1;
+}
+
+struct Node *buildTree(int postOrder[],int inOrder[],int start,int end){
+    if(start > end){
+        return nullptr;
+    }
+    static int index = end;
+    int curr = inOrder[index];
+    struct Node *node = new Node(curr);
+    index --;
+    
+    if(start == end){
+        return node;
+    }
+    int pos = search_pos(inOrder,start,end,curr);
+    node->right = buildTree(postOrder,inOrder,pos+1,end);
+    node->left = buildTree(postOrder,inOrder,0,pos-1);
+
+    return node;
+}
+
+int main(){
+    cout<<"\n";
+
+    int postOrder[] = {4,2,5,3,1};
+    int inOrder[] = {4,2,1,5,3};
+
+    struct Node *root = buildTree(postOrder,inOrder,0,4);
+    inOrderTraversal(root);
+    return 0;
+}
